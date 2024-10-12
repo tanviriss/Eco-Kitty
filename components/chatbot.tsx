208024021bd058d806744ee7367fef6df5856980
Evandabest <evanhaque1@gmail.com>
2024-10-12 17:47:45 -0400

    added shadcn comps for ui

---
+++ components/chatbot.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function DialogCloseButton() {
+  return (
+    <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+            </DialogDescription>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
+
+  )
+}
